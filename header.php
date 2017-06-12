5ae4853b774aad2c323a8cab2c5b852e94ca2d5c
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-06-12 13:15:04 +0300

    add social menu

--- header.php
+++ header.php
@@ -14,7 +14,24 @@
       <a href="<?php echo esc_url(home_url('/')); ?>">
         <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="Logo image" class="logoimage">
       </a>
+    </div><!-- logo -->
+    <div class="header-information">
+      <div class="socials">
+        <?php
+          $args = array(
+            'theme_location'  => 'social-menu',
+            'container'       => 'nav',
+            'container_class' => 'socials'
+          );
+          wp_nav_menu($args);
+        ?>
+      </div><!-- socials -->
+      <div class="address">
+        <p>8179 Bay Avenue Mountain View, CA 94043</p>
+        <p>Phone Number: +1-92-456-7890</p>
+      </div>
     </div>
+
   </header>
 
   <div class="main-menu">
@@ -22,7 +39,7 @@
       <?php
         $args = array(
           'theme_location'  => 'header-menu',
-          'container'       => 'mav',
+          'container'       => 'nav',
           'container_class' => 'site-nav'
         );
         wp_nav_menu($args);
