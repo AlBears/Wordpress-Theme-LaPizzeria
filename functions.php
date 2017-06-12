a416712693f7313d25a37d5f7aaf74b53c173c68
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-06-12 15:50:10 +0300

    add font awesome

--- functions.php
+++ functions.php
@@ -4,9 +4,11 @@
 function lapizzeria_styles() {
 
   wp_register_style('normalize', get_template_directory_uri(). '/css/normalize.css', array(), '7.0.0' );
-  wp_register_style('style', get_template_directory_uri(). '/style.css', array('normalize'), '1.0' );
+  wp_register_style('font-awesome', get_template_directory_uri(). '/css/font-awesome.css', array(), '4.7.0' );
+  wp_register_style('style', get_template_directory_uri(). '/style.css', array('normalize', 'font-awesome'), '1.0' );
 
   wp_enqueue_style('normalize');
+  wp_enqueue_style('font-awesome');
   wp_enqueue_style('style');
 }
 
