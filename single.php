a365bf012c09d5cd730257f916c00e010d566fe2
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-06-21 13:51:59 +0300

    single post template

--- single.php
+++ single.php
@@ -1,1 +1,34 @@
-<h1>Hello from single</h1>
+<?php get_header();?>
+
+  <?php while(have_posts()): the_post(); ?>
+
+    <div class="hero" style="background-image:url(<?php echo get_the_post_thumbnail_url(); ?>)">
+      <div class="hero-content">
+        <div class="hero-text">
+          <h2><?php the_title(); ?></h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-content container">
+      <main class="content-text">
+        <div class="entry-information clear">
+          <div class="date">
+            <time>
+              <?php echo the_time('d'); ?>
+              <span><?php echo the_time('M'); ?></span>
+            </time>
+          </div>
+          <p class="author">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <?php the_author(); ?>
+          </p>
+        </div>
+
+        <?php the_content(); ?>
+      </main>
+    </div>
+
+  <?php endwhile; ?>
+
+<?php get_footer();?>
